a71153bbba471caef5c4866e00fa8d13df24c8fb
Igor Golovko <golovko1978@gmail.com>
2022-03-24 15:43:01 -0600

    added mutual char [], added method

--- src/CaesarCipherTypes.java
+++ src/CaesarCipherTypes.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public interface CaesarCipherTypes {
     //interface for different code/decode classes
-    void getCaesarCoreType();
+
+    char[] Alphabet = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+            'p','q','r','s','t','u','v','w','x','y','z', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+    void runCaesarCoreType();
+
+    void executeCaesarCoreType();
 
 }
